a97441be16ee83bdd3bbf416de4ebe8a2ceab6d7
Henry <henrstep@amazon.com>
2019-11-19 16:06:48 -0500

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Welcome from './components/welcome/Welcome';
 import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact'
 
+import Navigation from './components/navigation/Navigation';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Welcome name="Stephanie" />
+      {/* <Welcome name="Stephanie" />
       <Clock />
+      <Contact /> */}
+      <Navigation />
+      <Route exact path="/" render={(props)=><Welcome {...props} name="Stephanie"/>} />
+      <Route path="/clock" component={Clock} />
+      <Route path="/contact" component={Contact} />
     </div>
   );
 }
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 function Welcome(props) {
     return (
         <div>
