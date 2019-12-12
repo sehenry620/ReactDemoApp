f5f57f5c73ee5f6eb7d1fef72b489503dbf5c78c
Henry <henrstep@amazon.com>
2019-12-12 16:53:23 -0500

    update Jeopardy game

--- src/components/jeopardy/Jeopardy.js
+++ src/components/jeopardy/Jeopardy.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import our service
 import JeopardyService from "../../jeopardyService";
 class Jeopardy extends Component {
   //set our initial state and set up our service as this.client on this component
-  constructor(props){
+  constructor(props) {
     super(props);
     this.client = new JeopardyService();
     this.state = {
       data: {},
       score: 0
-    }
+    };
   }
   //get a new random question from the API and add it to the data object in state
   getNewQuestion() {
     return this.client.getQuestion().then(result => {
       this.setState({
         data: result.data[0]
-      })
-    })
+      });
+    });
   }
   //when the component mounts, get a the first question
   componentDidMount() {
@@ -25,13 +25,20 @@ class Jeopardy extends Component {
   }
   //display the results on the screen
   render() {
-    return (
-      <div>
-        Question: {this.state.data.question}<br/>
-        Value: {this.state.data.value}<br/>
-        
-      </div>
-    );
+    if (this.state.data.category) {
+      return (
+        <div>
+          Question: {this.state.data.question}
+          <br />
+          Category: {JSON.stringify(this.state.data.category.title)}
+          <br />
+          Value: {this.state.data.value}
+          <br />
+        </div>
+      )
+    } else {
+      return <div></div>;
+    }
   }
 }
 export default Jeopardy;
